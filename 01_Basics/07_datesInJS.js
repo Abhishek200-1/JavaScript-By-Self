9621aa810c4f957f1174664998a6c3394835abbf
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-03-01 17:16:41 +0530

    dates

---
+++ 01_Basics/07_datesInJS.js
@@ -0,0 +1,27 @@
+// dates
+
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toJSON())
+// console.log(typeof myDate)
+
+
+// let createdDate = new Date("04-22-2001")
+// console.log(createdDate)
+// console.log(createdDate.toLocaleDateString())
+
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp)
+// console.log(Math.floor(Date.now()/1000))
+
+
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth() + 1)
+console.log(newDate.getDate())
+
