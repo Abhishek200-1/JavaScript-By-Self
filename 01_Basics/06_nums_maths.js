8a9e75067b2b2957bd9f71d3472feab4cf4f9c2f
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-03-01 16:46:47 +0530

    MathandNum

---
+++ 01_Basics/06_nums_maths.js
@@ -0,0 +1,39 @@
+// const score = 400
+
+// const balance = new Number(400)
+// console.log(balance);
+
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+
+// const anotherNumber = 23.8966
+
+// console.log(anotherNumber.toPrecision(3))
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString())
+
+
+
+
+//************************************************************************Maths*******************************************************************/
+
+console.log(Math)
+console.log(Math.abs(-56))
+console.log(Math.ceil(4.5))
+console.log(Math.min(4,3,6,8,9,5,6,56,25,45))
+console.log(Math.max(4,3,6,8,9,5,6,56,25,45))
+
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
