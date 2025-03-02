ddf13edb848b7ffd7a5554d31ee973b49752e1cf
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-03-02 23:37:09 +0530

    array

---
+++ 02_Basics/02_Array.js
@@ -0,0 +1,9 @@
+const marvel_heros = ["thor", "ironman", "captainAmerica", "Hulk"]
+const dc_heros = ["batman", "superman", "flash", "wonderWomen"]
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+
+
+const allheros = marvel_heros.concat(dc_heros);
+console.log(allheros)
