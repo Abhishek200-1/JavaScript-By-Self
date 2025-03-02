187f5c6a3292d426017bc1a4d33cff1cb8fe7f1d
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-03-02 23:34:55 +0530

    array

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,43 @@
+let myArray = [0, 2, 3, 4, 5, 6, "Abhishek"]
+let myHeroes = ["shaktimaan", "hulk", "thor"]
+
+let myArray2 = new Array(1, 2, 3, 4, 5, 6)
+
+
+
+
+
+console.log(myArray[5])
+
+
+
+
+// array methods 
+
+
+// push add elements in array 
+myArray.push(6)
+myArray.push(9)
+// pop remove last elemnts of array
+myArray.pop()
+// unshift adds elemts in the start of array
+myArray.unshift(0)
+// removes elemnts from start
+myArray.shift()
+// includes shows that elements is present in array or not in boolean
+console.log(myArray.includes(2))
+// index of for elemnts index number
+console.log(myArray.indexOf(5))
+
+
+const newArray = myArray.join
+
+
+// slice , splice
+
+console.log("A", myArray)
+
+
+
+console.log(myArray)
+
