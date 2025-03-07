19c2bf6fd6acc96b15836511e3cc82598bffdd1b
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-03-07 19:35:49 +0530

    functions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,26 @@
+function sayMyName() {
+  console.log("A");
+  console.log("B");
+  console.log("H");
+  console.log("I");
+  console.log("S");
+  console.log("H");
+  console.log("E");
+  console.log("K");
+}
+
+// sayMyName(); 
+
+
+// function addTwoNumbers(num1, num2){
+//     console.log(num1 + num2)
+// }
+
+function addTwoNumbers(num1, num2){
+    let result = num1 + num2
+    return result
+    
+}
+
+const result = addTwoNumbers(3,5)
+console.log("result: ", result);
