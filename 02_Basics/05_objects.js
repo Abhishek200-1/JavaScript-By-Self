d5a4962481cde0ef21055a0eeb984b688c6c2a3b
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-03-07 19:11:05 +0530

    json api

---
+++ 02_Basics/05_objects.js
@@ -0,0 +1,21 @@
+// tree sturctu
+
+const course = {
+    courseName:"js hindi",
+    price: 199,
+    courseInstracture:"Abhishek",
+}
+
+console.log(course.courseInstracture)
+
+const {courseInstracture} = course
+
+console.log(courseInstracture)
+
+
+// json 
+// {
+//     "name":"Abhishek",
+//     "courseName":"js hindi",
+//     "price":"free"
+// }
